292d8f7146f1412818c27974b238fd1c92c56155
9ytshade <flynnbright@gmail.com>
2026-01-20 16:27:23 +0100

    first push commit

--- exo_03.rb
+++ exo_03.rb
@@ -1,7 +1,7 @@
 #code that asks for a user's year of birth and prints their age in 2017 to the terminal
 puts "What is your year of birth ?"
 
-birth_year = gets.chomp.to.i
+birth_year = gets.chomp.to_i
 year_to_calc = 2017
 
 age_in_2017 = year_to_calc - birth_year
